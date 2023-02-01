fa43e74b11845a7857901c0bdbf951da515e1087
franxxdmsk132 <122382819+franxxdmsk132@users.noreply.github.com>
2023-01-31 20:17:25 -0500

    Add files via upload

---
+++ JavaTimeEjercicio/src/javatimeejercicio/JavaTimeEjercicio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javatimeejercicio;
+
+/**
+ *
+ * @author frank
+ */
+public class JavaTimeEjercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JavaTime Pantalla = new JavaTime();
+        Pantalla.setVisible(true);
+    }
+
+}
